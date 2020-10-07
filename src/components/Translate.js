73c840b722693a63903d0590cf019b48f3e03bfd
Josue Lorenzo <josuelorenzo21@gmail.com>
2020-10-07 14:02:02 -0300

    Translate component was created and runing api

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,6 +1,7 @@
-//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+//
 import React, {useState} from "react";
 import Dropdown from "./Dropdown";
+import Convert from "./Convert";
 
 const options = [
     {
@@ -34,6 +35,9 @@ const Translate = () => {
             selected={language}
             onSelectedChange={setLanguage}
             options={options}/>
+            <hr></hr>
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 };
